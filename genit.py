83548af8a95be38b39ec41e7a21e3b05fa08958a
Tom Sapletta <tom@sapletta.com>
2024-07-15 10:39:57 +0200

    new release

---
+++ genit.py
@@ -0,0 +1,36 @@
+def generate_init_file(package_name, modules, ppath = ""):
+    content = f"""# Auto-generated __init__.py for {package_name} package
+
+# Import necessary modules
+{chr(10).join(f"from . import {module}" for module in modules)}
+
+# Public API of the package
+__all__ = {str(modules)}
+
+# Version checking
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("{package_name}")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+
+# You can add any initialization code here if needed
+"""
+
+    if not ppath:
+        ppath = "src/" + package_name + "/"
+
+    with open(ppath, 'w') as f:
+        f.write(content)
+
+
+# Usage
+import os
+from pathlib import Path
+package_name = "pyfunc_config"
+directory = "src/" + package_name + "/"
+py_files = [f for f in os.listdir(directory) if f.endswith('.py') and f != '__init__.py']
+modules = [os.path.splitext(f)[0] for f in py_files]
+generate_init_file('pyfunc_config', modules)
--- src/pyfunc_config/get_email_path.py
+++ src/pyfunc_config/get_email_path.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 def get_email_path(email_target, storage_root):
     p = Path(storage_root).expanduser()
     # print(email)
